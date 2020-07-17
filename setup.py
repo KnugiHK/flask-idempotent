c36cf0d3a6ef227f1e56ea596ceb05579141b9eb
KnugiHK <24708955+KnugiHK@users.noreply.github.com>
2020-07-17 16:54:34 +0800

    Update setup.py for later release in pypi

--- setup.py
+++ setup.py
@@ -11,21 +11,20 @@ with open("README.rst") as d:
     __doc__ = d.read()
 
 setup(
-    name='Flask-Idempotent',
-    version='0.1.0',
-    url='http://github.com/DisruptiveLabs/flask-idempotent',
+    name='Flask-Idempotent-Memory',
+    version='0.1.1',
+    url='https://github.com/KnugiHK/flask-idempotent-memory',
     license='MIT',
-    author='Franklyn Tackitt',
-    author_email='frank@comanage.com',
-    description='Idempotent requests for Flask applications',
+    author='Knugi (originally Flask-Idempotent by Franklyn Tackitt)',
+    author_email='info@knugi.xyz',
+    description='Idempotent requests for Flask applications using memory only',
     long_description=__doc__,
     py_modules=['flask_idempotent'],
     zip_safe=False,
     include_package_data=True,
     platforms='any',
     install_requires=[
-        'Flask>=0.9',
-        'redis'
+        'Flask>=0.9'
     ],
     tests_require=TEST_DEPS,
     extras_require={'tests': TEST_DEPS},
